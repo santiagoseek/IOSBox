80f2c23b2a0d432aaed2779e2c62a313915f35e6
zkk赵珂珂 <kkzhao@DST60177.local>
2015-01-04 20:31:23 +0800

    this is testing for commit

--- signalViewApp/ViewController.h
+++ signalViewApp/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+-(void)NSstringTest;
 
 @end
 
--- signalViewApp/ViewController.m
+++ signalViewApp/ViewController.m
@@ -24,4 +24,148 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(IBAction)testFun:(id)sender{
+    NSString *str = @"111.01";
+    id result = [NSNumber numberWithFloat:[str floatValue]];
+    NSLog(@"%f", [str floatValue]);
+    NSLog(@"%f", [result floatValue]);
+    
+    [self NSMutableStringTest];
+}
+
+-(IBAction)NSstringTest:(id)sender{
+    NSString *str1 = @"this is string A";
+    NSString *str2 = @"this is string B";
+    NSString *res;
+    NSComparisonResult compareResult;
+    
+    NSLog(@"Length of str1: %lu", [str1 length]);
+    
+    
+    //字符串复制
+    res=[NSString stringWithString:str1];
+    NSLog(@"copy:%@",res);
+    if ([str1 isEqualToString:res] == YES) {
+        NSLog(@"str1 == res");
+    }
+    else
+    {
+        NSLog(@"str1 != res");
+    }
+    
+    //str2放在str1的末尾
+    str2 = [str1 stringByAppendingString:str2];
+    NSLog(@"concatentation: %@", str2);
+    
+    //格式化输出
+    NSString *newString = [NSString stringWithFormat:@"%@%@", str1,str2];
+    NSLog(@"stringWithFormat: %@", newString);
+    
+    //验证一个字符串是否小于，等于或大于另一个字符串
+    compareResult = [str1 compare:str2];
+    if (compareResult == NSOrderedAscending) {
+        NSLog(@"str1 < str2");
+    }
+    else if (compareResult == NSOrderedSame) {
+        NSLog(@"str1 == str2");
+    }
+    else{
+        NSLog(@"str1 > str2");
+    }
+    
+    //字符串转化为大写
+    res = [str1 uppercaseString];
+    NSLog(@"uppercase conversion:%s", [res UTF8String]);
+    
+    //字符串转化为小写
+    res = [str1 lowercaseString];
+    NSLog(@"lowercase conversion:%@", res);
+    
+    NSLog(@"original string:%@", str1);
+    
+    //获取字符串对应的C风格字符串
+    const char *cstr = [str1 UTF8String];
+    NSLog(@"获取的c字符串:%s", cstr);
+    
+    //在str1后面追加带变量的字符串
+    //原来的字符串对象并不改变，只是将新生成的字符串重新赋给str1指针变量
+    str1 = [str1 stringByAppendingFormat:@"%@ str1.",str1];
+    NSLog(@"str1 is:%@", str1);
+    NSLog(@"Length of the str1:%lu",[str1 length]);
+    
+    //获取str的前10个字符组成的字符串
+    NSString *s1 = [str1 substringToIndex:10];
+    NSLog(@"str1 substringToIndex10:%@", s1);
+    
+    //获取str从第5个字符开始，与后面字符组成的字符串
+    NSString *s2 = [str1 substringFromIndex:5];
+    NSLog(@"str1 substringFromIndex5:%@", s2);
+    
+    //获取str从第5个字符开始，到第15个字符组成的字符串
+    NSString *s3 = [str1 substringWithRange:NSMakeRange(5, 15)];
+    NSLog(@"substringWithRange:NSMakeRange(5, 15):%@", s3);
+    
+    //获取"Athis"在str1中出现的位置
+    NSRange pos = [str1 rangeOfString:@"Athis"];
+    NSLog(@"Athis start position:%ld, the length is:%ld", pos.location, pos.length);
+}
+
+-(void)NSstringTest{
+    NSLog(@"this is testing fun override.");
+}
+
+-(void)NSMutableStringTest{
+    NSString *str1 = @"this is nsmutablestring A";
+    NSMutableString *mstr;
+    
+    //从不可变字符串创建可变字符串
+    mstr = [NSMutableString stringWithString:str1];
+    NSLog(@"NSMutableString:%@", mstr);
+    
+    //插入字符
+    [mstr insertString:@"insert" atIndex:4];
+    NSLog(@"insert the \"insert\" at the 4:%@",mstr);
+    
+    //插入末尾进行有效拼接
+    [mstr insertString:@" and string B" atIndex:[mstr length]];
+    NSLog(@"the end insert:%@", mstr);
+    
+    //appendString
+    [mstr appendString:@" and string C"];
+    NSLog(@"appendString:%@", mstr);
+    
+    //根据范围删除子字符串
+    [mstr deleteCharactersInRange:NSMakeRange(0, 7)];
+    NSLog(@"deleteCharaterInrange:%@", mstr);
+    
+    //查找然后删除
+    NSRange substr = [mstr rangeOfString:@"string B and "];
+    if (substr.location != NSNotFound) {
+        [mstr deleteCharactersInRange:substr];
+        NSLog(@"find and delete the \"string B and\":%@", mstr);
+    }
+    
+    //直接设置为可变的字符串
+    [mstr setString:@"this is new string A"];
+    NSLog(@"the new mstr:%@",mstr);
+    
+    [mstr replaceCharactersInRange:NSMakeRange(8, 6) withString:@" a mutable string"];
+    NSLog(@"replaceCharactersInRange:%@", mstr);
+    
+    NSLog(@"mstr:%@", mstr);
+    
+    NSString *search = @"a";
+    NSString *replace = @"X";
+    
+    substr = [mstr rangeOfString:search];
+    if (substr.location != NSNotFound) {
+        [mstr replaceCharactersInRange:substr withString:replace];
+    }
+    NSLog(@"replace a with X:%@",mstr);
+}
+
+
+
+
 @end
