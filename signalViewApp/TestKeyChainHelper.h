69dac0f20ca2259e2b2dcb4e15e4dc2e1c8bed1e
santiago <alex-zhao76@hotmail.com>
2015-01-09 19:44:34 +0800

    Create TestKeyChainHelper.h

---
+++ signalViewApp/TestKeyChainHelper.h
@@ -0,0 +1,31 @@
+//
+//  TestKeyChainHelper.h
+//  signalViewApp
+//
+//  Created by test on 15-1-9.
+//  Copyright (c) 2015年 zkk赵珂珂. All rights reserved.
+//
+
+#ifndef signalViewApp_TestKeyChainHelper_h
+#define signalViewApp_TestKeyChainHelper_h
+
+
+#endif
+
+#import <Foundation/Foundation.h>
+
+@interface TestKeyChainHelper : NSObject
+
++ (void) saveUserName:(NSString*) userName
+      userNameService:(NSString*) userNameService
+             password:(NSString*) pwd
+      passwordService:(NSString*) pwdService;
+
++ (void) deleteWithUserNameService:(NSString*) userNameService
+                   passwordService:(NSString*) pwdService;
+
++(NSString*) getUserNameWithService:(NSString*) userNameService;
+
++(NSString*) getPasswordWithService:(NSString*) pwdService;
+
+@end
